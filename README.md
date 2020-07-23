58560cfc4859e65c8116f05a12125536a6e2ee4d
nilsboy <nilsb@boysen.info>
2020-07-23 16:08:10 +0200

    only report the same error once; add tests

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Add vim's errorformat as prefix to lines matching a regex.
+
+See `errorformatregex --help` for details.
---
+++ test/main.test.js
@@ -0,0 +1,92 @@
+const getStream = require(`get-stream`)
+const spawn = require("child_process").spawn
+const assert = require(`assert`)
+
+async function run(given, options) {
+  const child = spawn("bin/errorformatregex", options)
+  child.stdin.write(given)
+  child.stdin.end()
+  child.stderr.pipe(process.stderr)
+  return getStream(child.stdout)
+}
+
+test(`no error`, async () => {
+  const given = `MyError: foo bar (754:10)`
+  const expected = given
+
+  assert.equal(await run(given, []), expected)
+})
+
+test(`error without location`, async () => {
+  const given = `MyError: foo bar (754:10)`
+  const expected = `errorformatregex: :0:0:e:MyError: foo bar (754:10)`
+
+  assert.equal(await run(given, [`e/error/igm`]), expected)
+})
+
+test(`with filename`, async () => {
+  const given = `MyError: test/main.test.js: foo bar (754:10)`
+  const expected = `errorformatregex:test/main.test.js:0:0:e:MyError: test/main.test.js: foo bar (754:10)`
+
+  assert.equal(
+    await run(given, [`e/error.*?([\\w/.\\-@-]+\\/[\\w./\\-@-]+)/igm`]),
+    expected
+  )
+})
+
+test(`with location`, async () => {
+  const given = `MyError: test/main.test.js: foo bar (754:10)`
+  const expected = `errorformatregex:test/main.test.js:754:10:e:MyError: test/main.test.js: foo bar (754:10)`
+
+  assert.equal(
+    await run(given, [
+      `e/error.*?([\\w/.\\-@]+\\/[\\w./\\-@]+).*?(\\d+)\\:(\\d+)/igm`,
+    ]),
+    expected
+  )
+})
+
+test(`prefer last matching regex`, async () => {
+  const given = `MyError: test/main.test.js: foo bar (754:10)`
+  const expected = `errorformatregex:test/main.test.js:0:0:e:MyError: test/main.test.js: foo bar (754:10)`
+
+  assert.equal(
+    await run(given, [
+      `e/error/igm`,
+      `e/error.*?([\\w/.\\-@]+\\/[\\w./\\-@]+)/igm`,
+    ]),
+    expected
+  )
+})
+
+test(`only match first of two matching lines of same location`, async () => {
+  const given = `
+MyError: first: foo bar
+MyError: second: foo bar
+`
+  const expected = `
+errorformatregex: :0:0:e:MyError: first: foo bar
+MyError: second: foo bar
+`
+
+  assert.equal(await run(given, [`e/error/ig`]), expected)
+})
+
+test(`use last matching regex`, async () => {
+  const given = `
+MyError: test/main.test.js: foo bar (754:10)
+MyError: test/main.test.js: foo bar (754:10)
+  `
+  const expected = `
+errorformatregex:test/main.test.js:0:0:e:MyError: test/main.test.js: foo bar (754:10)
+MyError: test/main.test.js: foo bar (754:10)
+  `
+
+  assert.equal(
+    await run(given, [
+      `e/error.*$/igm`,
+      `e/error.*?([\\w/.\\-@]+\\/[\\w./\\-@]+)/igm`,
+    ]),
+    expected
+  )
+})
